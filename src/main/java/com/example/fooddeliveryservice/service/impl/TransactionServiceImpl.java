6ea4fb9667e93b4ce7a5c29ec531d695bc1726a7
Hansani Perera <Hansani.Perera@axiatadigitallabs.com>
2022-02-12 02:45:46 +0530

    Api implementation started

---
+++ src/main/java/com/example/fooddeliveryservice/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.fooddeliveryservice.service.impl;
+
+import com.example.fooddeliveryservice.entity.TransactionDetails;
+import com.example.fooddeliveryservice.service.TransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    public void performTransaction(TransactionDetails transactionDetails) {
+        //test
+    }
+}
